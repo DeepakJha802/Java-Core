f6d8faf1e4cfcd66307967d36ab0f8fa0a8419e9
DeepakJha802 <aec.cse.deepak@gmail.com>
2023-08-23 01:57:17 +0530

    Changes

--- Solution.java
+++ Solution.java
@@ -2,6 +2,7 @@ class A                        // This is Parent Class
 {
     public A()
     {
+        // this(5);
         System.out.println("This is Default Constructor of Class A");
     }
     public A(int n)
@@ -9,16 +10,34 @@ class A                        // This is Parent Class
         System.out.println("This is Parameterise Constructor of Class A");
     }
 }
-class B extends A             // This is Sub-Class
+class B extends A            // This is Sub-Class
 {
     public B()
     {
+        // this(5);
         System.out.println("This is Default constructor of Class B");
     }
+
     public B(int n)
     {
-        this();
         System.out.println("This is Parameterise Constructor of Class B");
+    }
+
+    public void print()
+    {
+        System.out.println("This is Method of the class B");
+    }
+}
+
+class C extends B
+{
+    public C()
+    {
+        System.err.println("This is Default Constructor of Class C");
+    }
+    public C(int n)
+    {
+        System.out.println("This is Parameterise Constructor of Class C");
     }
 }
 
@@ -26,6 +45,8 @@ class Solution
 {
     public static void main(String[] args)
     {
-        B ob = new B(10);
+        B ob = new B();
+        ob.print();
+        // B obj = new B(5);
     }
 }
